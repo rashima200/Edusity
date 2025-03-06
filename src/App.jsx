728c2ea9cd602df0a9b08ba3a2be550eae257320
rashima200 <rashimarasangi200@gmail.com>
2025-03-06 17:47:32 +0530

    feat:program section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
+import Programs from './components/Programs/Programs'
 const App = () => {
   return (
     <div>
      <Navbar/>
      <Hero/>
+     <div className="container">
+     <Programs/>
+     </div>
     </div>
   )
 }
