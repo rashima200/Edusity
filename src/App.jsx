437bf187eb90a6a7cef4e7559028eb2a54cda582
rashima200 <rashimarasangi200@gmail.com>
2025-03-08 18:52:19 +0530

    feat:footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import About from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
-
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
@@ -24,6 +24,7 @@ const App = () => {
      <Testimonials/>
      <Title subTitle='CONTACT US' title='Get In Touch'/>
      <Contact/>
+     <Footer/>
      </div>
     </div>
   )
